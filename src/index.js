3be546059f4dec90d7b47b581bc5142d849517d6
Syarif Amhar <syarifamhar@gmail.com>
2024-11-11 12:04:50 +0700

    vercel analytics fixed

--- src/index.js
+++ src/index.js
@@ -9,6 +9,7 @@ import { Analytics } from "@vercel/analytics/react";
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <Analytics />
     <App />
   </React.StrictMode>
 );
